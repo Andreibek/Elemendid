27adaf203667a13a5c3789c67c0d9ce35215963a
o <o>
2022-04-29 10:43:37 +0300

    f

--- Elemendid_app/List_Page.xaml.cs
+++ Elemendid_app/List_Page.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,9 +15,10 @@ namespace Elemendid_app
     public partial class List_Page : ContentPage
     {
 
-        public List<Telefon> telefons { get; set; }
+        public ObservableCollection<Telefon> telefons { get; set; }
         Label lbl_list;
         ListView list;
+        Button lisa, kustuta;
         public List_Page()
         {
             lbl_list = new Label
@@ -25,7 +27,7 @@ namespace Elemendid_app
                 HorizontalOptions = LayoutOptions.Center,
                 FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label))
             };
-            telefons = new List<Telefon>
+            telefons = new ObservableCollection<Telefon>
             {
                 new Telefon{Nimetus="Samsung Galaxy S22 Ultra",Tootja="Samsung",Hind=1349},
                 new Telefon{Nimetus="Xiaomi Mi 11 Lite 5G NE",Tootja="Xiaomi",Hind=399},
@@ -34,6 +36,10 @@ namespace Elemendid_app
             };
             list = new ListView
             {
+                SeparatorColor = Color.Orange,
+                Header = "Minu oma kolektion:",
+                Footer = DateTime.Now.ToString("T"),
+
                 HasUnevenRows=true,
                 ItemsSource=telefons,
                 ItemTemplate=new DataTemplate(() =>
@@ -48,7 +54,26 @@ namespace Elemendid_app
                 })
             };
             list.ItemTapped += List_ItemTapped;
-            this.Content = new StackLayout { Children = { lbl_list, list } };
+            lisa = new Button { Text = "Lisa felefon" };
+            lisa.Clicked += Lisa_Clicked;
+            kustuta = new Button { Text = "Kustuta telefn" };
+            kustuta.Clicked += Kustuta_Clicked;
+            this.Content = new StackLayout { Children = { lbl_list, list, lisa, kustuta } };
+        }
+
+        private void Kustuta_Clicked(object sender, EventArgs e)
+        {
+            Telefon phone = list.SelectedItem as Telefon;
+            if (phone != null)
+            {
+                telefons.Remove(phone);
+                list.SelectedItem = null;
+            }
+        }
+
+        private void Lisa_Clicked(object sender, EventArgs e)
+        {
+            telefons.Add(new Telefon { Nimetus = "Telefon", Tootja = "Tootja", Hind = 1 });
         }
 
         private async void List_ItemTapped(object sender, ItemTappedEventArgs e)
